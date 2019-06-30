9b0064610620c17e9da09cf44b0e29471257a4fa
c1k4dutz666 <51845420+c1k4dutz666@users.noreply.github.com>
2019-06-30 11:42:47 +0700

    Create alas.py

---
+++ alas.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('Loading..............')
+mengetik('.')
+mengetik('.')
+mengetik('Mencoba meminta akses..')
+mengetik('..www.angkorliga.net................')
+mengetik('.sponsor by Angkorliga')
+mengetik('.Minimal deposit 20ribu')
+mengetik('......................................')
+mengetik('...mengupgrade')
+mengetik('...')
+mengetik('...')
+mengetik('mengupdate!')
+mengetik('100%')
+mengetik ('ydu+i674h6764778_3ijr9nee+990)')
+mengetik ('Reycuuuuuuuuu!')
